15f8e6cac3a74a0d3e5dee4d0ace59d71ff6b3de
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-08 11:52:00 +0300

    MRU caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,37 +4,31 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    '''MRUCache class that inherits from BaseCaching'''
+    """ MRUCache class that inherits from BaseCaching """
 
     def __init__(self):
-        '''Initialize'''
+        """ Initialize """
         super().__init__()
-        self.queue = []
 
     def put(self, key, item):
-        '''Add an item to the cache'''
+        """ Add an item to the cache """
         if key is None or item is None:
             return
 
-        if key in self.cache_data:
-            '''Move the key to the front of the queue'''
-            self.queue.remove(key)
-        elif len(self.cache_data) >= self.MAX_ITEMS:
-            '''Remove the most recently used item (MRU)'''
-            mru_key = self.queue.pop()
-            del self.cache_data[mru_key]
-            print('DISCARD:', mru_key)
-
         self.cache_data[key] = item
-        self.queue.insert(0, key)
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            mru_key = self.get_mru_key()
+            del self.cache_data[mru_key]
+            print("DISCARD:", mru_key)
 
     def get(self, key):
-        '''Get an item by key'''
+        """ Get an item by key """
         if key is None or key not in self.cache_data:
             return None
 
-        '''Move the key to the front of the queue'''
-        self.queue.remove(key)
-        self.queue.insert(0, key)
-
         return self.cache_data[key]
+
+    def get_mru_key(self):
+        """ Get the key of the most recently used item """
+        return next(reversed(self.cache_data))
