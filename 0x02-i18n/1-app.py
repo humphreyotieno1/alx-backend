2d4247c60a030bf7782c9ccbaa2d6a094d6df74f
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-18 00:26:42 +0300

    Basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-"""Basic flask application"""
-import babel
+"""flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
+app.config.from_object('Config')
 babel = Babel(app)
 
 
+@app.route('/')
+def single():
+    """renders a single html helloworld"""
+    return render_template('1-index.html')
+
+
 class Config:
-    """default langs"""
+    """default languages"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object('Config')
-
-
-@app.route("/", methods=['GET'], strict_slashes=False)
-def hello_world():
-    """render html page"""
-    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
