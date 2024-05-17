704546021e138a02635a1b81631b04791431206e
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-18 00:21:07 +0300

    Basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 """Basic flask application"""
+import babel
 from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object('Config')
 babel = Babel(app)
-
-
-@app.route("/")
-def hello_world():
-    """render html page"""
-    return render_template('1-index.html')
 
 
 class Config:
@@ -19,6 +13,15 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('Config')
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """render html page"""
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
